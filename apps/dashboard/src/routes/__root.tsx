807bed238ae903f2ec396a27377f9b20189a7458
astronautfa <145324740+astronautfa@users.noreply.github.com>
2025-03-17 17:11:52 +0330

    removed tailwind config

--- apps/dashboard/src/routes/__root.tsx
+++ apps/dashboard/src/routes/__root.tsx
@@ -1,6 +1,8 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
+import '@app/ui/globals.css'
+
 export const Route = createRootRoute({
   component: () => (
     <>
--- apps/dashboard/vite.config.js
+++ apps/dashboard/vite.config.js
@@ -6,10 +6,13 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
   },
-
 });
