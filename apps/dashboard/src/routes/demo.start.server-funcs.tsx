0a3d2af09338ed06034f031554f88b312c9b21a9
astronautfa <145324740+astronautfa@users.noreply.github.com>
2025-03-17 18:57:22 +0330

    Button changed to shadcn

--- apps/dashboard/src/routes/demo.start.server-funcs.tsx
+++ apps/dashboard/src/routes/demo.start.server-funcs.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@app/ui/components/button'
 import { createFileRoute } from '@tanstack/react-router'
 
 
@@ -10,13 +11,9 @@ function Home() {
 
   return (
     <div className="p-4">
-      <button
-        type="button"
-
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
+      <Button variant={'secondary'}>
         Add 1 to {state}?
-      </button>
+      </Button>
     </div>
   )
 }
